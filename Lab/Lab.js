b05b329701dc9b6465c664bf6e642c027cf8104b
Tahamina Chowdhury <tahaminachowdhury@Tahaminas-Air.home>
2022-10-06 12:18:27 -0400

    Added Barchart Lab

--- Lab/Lab.js
+++ Lab/Lab.js
@@ -1,18 +1,42 @@
-var can = document.getElementById("canv");
+var context = document.getElementById("myChart").getContext("2d");
 
-var context = can.getContext("2d");
+var fruits = [
+	{name:"Strawberry", quantity:15, color:"red"},
+	{name:"Peach", quantity:10, color:"orange"},
+  {name: "Banana", quantity:25, color:"yellow"},
+  {name: "Pear", quantity:10, color:"green"},
+  {name: "grape", quantity:20, color: "purple"}
+];
 
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 15];
-var barColors = ["red", "green","blue","orange","brown"];
+var names = [];
+var quantities = [];
+var colors = [];
+
+for (fruit of fruits) {
+  names.push(fruit.name);
+  quantities.push(fruit.quantity);
+  colors.push(fruit.color);
+}
 
 new Chart("myChart", {
   type: "bar",
   data: {
-    labels: xValues,
+    labels: names,
     datasets: [{
-      backgroundColor: barColors,
-      data: yValues
+      backgroundColor: colors,
+      data: quantities
     }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Quantities of Fruits"
+    },
+    scales: {
+      yAxes: [{
+        ticks: { beginAtZero: true}
+      }]
+    }
   }
 });
