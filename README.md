18c25fb39d83c9589649337d085202f58fb66932
Tahamina Chowdhury <tahaminachowdhury@Tahaminas-Air.home>
2022-11-27 13:42:57 -0500

    Adding the Text Form

---
+++ README.md
@@ -0,0 +1,6 @@
+CISC 3160 Introduction to Multimedia Programming Labs and Projects
+
+1. Cartoon (Project)
+2. Color Chart (Lab)
+3. Animation (Project) 
+4. Text Form (Project)
---
+++ Text Form/textForm.js
@@ -0,0 +1,16 @@
+function draw() {
+  setInterval(()=> {
+    let id = document.getElementById("id");
+    id.innerText = document.getElementById("fname").value;
+    document.body.style.backgroundColor = document.getElementById("backgroundColor").value;
+    id.style.color = document.getElementById("textColor").value;
+    id.style.fontSize = document.getElementById("slider").value+"px";
+},200);
+  var radios = document.forms["form"].elements["font"];
+  for(var i = 0, max = radios.length; i < max; i++) {
+      radios[i].onclick = function() {
+          id.style.fontFamily = this.value;
+      }
+  }
+}
+
